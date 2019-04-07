723bee5209f091600a34ff6fe8a3271b1429bae0
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 19:45:00 +0000

    Snapshot of branch master taken on 2019-04-07T15:44:59.731-04:00 from "https://github.com/UnofficialJuliaMirror/ChaosTools.jl-608a59af-f2a3-5ad4-90b4-758bdf3122a7"

---
+++ test/runtests.jl
@@ -0,0 +1,19 @@
+ti = time()
+
+# Continuous
+include("lyapunov_exponents.jl")
+include("chaos_detection_tests.jl")
+include("poincare_tests.jl")
+
+# Discrete
+include("orbitdiagram_tests.jl")
+include("periodicity_tests.jl")
+
+# Numeric
+include("entropy_dimension.jl")
+include("nlts_tests.jl")
+
+ti = time() - ti
+println("\nTest took total time of:")
+println(round(ti, digits = 33), " seconds or ",
+round(ti/60, digits = 3), " minutes")
